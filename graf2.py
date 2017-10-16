56a7b5b3cfea39e0654c9d3825a246617430e8d7
dev_wal <waugustojr@gmail.com>
2017-10-16 19:54:38 -0200

    Desenvolvimento Big Data

---
+++ graf2.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+plt.plot( [10,5,3,4,6,8] )
+plt.title("OK")
+plt.show()
---
+++ teste1.py
@@ -0,0 +1,12 @@
+arquivo = open("/home/walter/log.txt","r")
+pesquisa = raw_input("Digite o termo procurado: ")
+#teste print ipesquisa="teste5555555555555555"
+print (pesquisa)
+teste
+#for i in arquivo.readlines():
+	 #print i
+      #if i.count(teste) > 0:
+      # print i
+#print "nao encontrei o padrao"
+
+
