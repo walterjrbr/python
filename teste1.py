02e94c325c8968a1da07e621d36c9caedb073d1c
walterjrbr <waugustojr@gmail.com>
2018-06-20 15:46:33 -0300

    modified:   teste1.py

--- teste1.py
+++ teste1.py
@@ -1,7 +1,7 @@
 arquivo = open("/home/walter/log.txt","r")
 pesquisa = raw_input("Digite o termo procurado: ")
 #teste print ipesquisa="teste55555555555555"
-print (pesquisa_teste_ok_05)
+print (pesquisa_teste_ok_06)
 teste
 #for i in arquivo.readlines():
 	 #print i
